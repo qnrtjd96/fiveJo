e5f9220bfefa3579c0fa3996201025569408d83d
YULCOM <YULCOM@DESKTOP-VIRT8D9>
2021-01-06 16:57:54 +0900

    20.1.6-serah

---
+++ Test/src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public Test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Test - serah");
+
+	}
+
+}
